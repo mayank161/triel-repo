819d9e0c9d86c23c0f7e113cc6713409430f28a5
Mayank <satimayank74@gmail.com>
2021-03-02 04:57:06 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let topic = "you had opened the console";
+console.log(topic);
+
+// swapping two numbers without taking new variable
+
+let num1 = 10;
+let num2 = 20;
+
+console.log("num1 = " + num1 + " & num2 = " + num2);
+
+num1 += num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+
+console.log("num1 = " + num1 + " & num2 = " + num2);
